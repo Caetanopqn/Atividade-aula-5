e3701f528083f2a9d230edf47d93adbc0ddd37ce
Caetanopqn <115043671+Caetanopqn@users.noreply.github.com>
2026-04-13 11:27:43 -0300

    Create algoritmo07.py

---
+++ algoritmo07.py
@@ -0,0 +1,11 @@
+#maior ou menor que 10
+
+n = int(input("digite um valor: "))
+
+if n == 10:
+    print("esse valor é 10")
+
+if n > 10:
+    print("é maior que 10")
+else:
+    print("é menor que 10")

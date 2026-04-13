6a87013ff7001e8fb46bd587c9bd7299883609f9
Caetanopqn <115043671+Caetanopqn@users.noreply.github.com>
2026-04-13 10:38:43 -0300

    Create algoritmo01.py

---
+++ algoritmo01.py
@@ -0,0 +1,11 @@
+#positivo ou negativo
+
+n = float(input("digite um valor: "))
+
+if n > 0:
+    print("esse valor é positivo")
+elif n < 0:
+    print("esse valor é negativo")
+
+if n == 0:
+    print("esse valor é neutro")

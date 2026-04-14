2b72662232293eec24b1676f080e6f87075cefab
Caetanopqn <115043671+Caetanopqn@users.noreply.github.com>
2026-04-14 13:36:03 -0300

    Create algoritmo10.py

---
+++ algoritmo10.py
@@ -0,0 +1,8 @@
+#entre 0 e 10
+
+n = int(input("digite um valor: "))
+
+if n >= 0 and n <= 10:
+    print("esse valor está dentro do intervalo")
+else:
+    print("esse valor está fora do intervalo ")

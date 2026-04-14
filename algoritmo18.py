d4977fc635cb2a67b2fd2b51bcfe8e4fe700d21f
Caetanopqn <115043671+Caetanopqn@users.noreply.github.com>
2026-04-14 14:42:32 -0300

    Create algoritmo18.py

---
+++ algoritmo18.py
@@ -0,0 +1,14 @@
+#par +/- ou ímpar +/-
+
+n = int(input("Digite um valor: "))
+
+if n == 0:
+    print("esse valor é neutro")
+elif n % 2 == 0 and num > 0:
+    print("esse valor é par positivo")
+elif n % 2 == 0 and num < 0:
+    print("esse valor é par negativo")
+elif n % 2 != 0 and num > 0:
+    print("esse valor é ímpar positivo")
+else:
+    print("esse valor é ímpar negativo")

f625ca34240bea93e8c1e1844333977fb92abbf1
Caetanopqn <115043671+Caetanopqn@users.noreply.github.com>
2026-04-14 14:48:31 -0300

    Create algoritmo19.py

---
+++ algoritmo19.py
@@ -0,0 +1,10 @@
+#subtração de valores
+
+n1 = float(input("digite o 1º valor: "))
+n2 = float(input("digite o 2º valor: "))
+
+if n1 == n2:
+    print("os números são iguais")
+else:
+    print("os números são diferentes")
+    print("diferença:", n1 - n2)

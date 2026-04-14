f4090de1a054cfb1c2368f65b884f799ba81cff0
Caetanopqn <115043671+Caetanopqn@users.noreply.github.com>
2026-04-14 14:50:43 -0300

    Update algoritmo18.py

--- algoritmo18.py
+++ algoritmo18.py
@@ -4,11 +4,11 @@ n = int(input("Digite um valor: "))
 
 if n == 0:
     print("esse valor é neutro")
-elif n % 2 == 0 and num > 0:
+elif n % 2 == 0 and n > 0:
     print("esse valor é par positivo")
-elif n % 2 == 0 and num < 0:
+elif n % 2 == 0 and n < 0:
     print("esse valor é par negativo")
-elif n % 2 != 0 and num > 0:
+elif n % 2 != 0 and n > 0:
     print("esse valor é ímpar positivo")
 else:
     print("esse valor é ímpar negativo")

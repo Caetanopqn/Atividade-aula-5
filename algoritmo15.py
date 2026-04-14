e5c8e01764e323086cff2805956660ab38bc42bb
Caetanopqn <115043671+Caetanopqn@users.noreply.github.com>
2026-04-14 14:15:50 -0300

    Create algoritmo15.py

---
+++ algoritmo15.py
@@ -0,0 +1,8 @@
+#entre 10 e 20
+
+n = int(input("digite um valor: "))
+
+if n >= 10 and n <= 20:
+    print("esse valor está dentro do intervalo")
+else:
+    print("esse valor está fora do intervalo ")
